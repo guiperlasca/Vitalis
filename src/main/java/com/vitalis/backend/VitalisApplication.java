da7cdff2a9593605cd1a4ef8674a42f044aad27f
Guilherme Perlasca <perlasca47@gmail.com>
2025-11-21 17:54:49 -0300

    Subindo Projeto Vitalis

---
+++ src/main/java/com/vitalis/backend/VitalisApplication.java
@@ -0,0 +1,13 @@
+package com.vitalis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalisApplication.class, args);
+	}
+
+}
